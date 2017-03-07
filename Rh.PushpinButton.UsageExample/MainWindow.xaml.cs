33e57b18aced7f939dd041c8ad97a1ad2621e5ff
Rollinghours <rhours@outlook.com>
2017-03-07 04:14:16 +0300

    Initial release

---
+++ Rh.PushpinButton.UsageExample/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Rh.PushpinButton.UsageExample {
+
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+    }
+}
